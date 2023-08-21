1afd0d02c4d783ea8798719eb93f55a3346a33a0
Sergio Henriques <sergio@sudoserv.com>
2023-08-21 11:53:34 +0200

    adding a more appropriate boilterplate

--- main.go
+++ main.go
@@ -1,29 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
-func main() {
-	// Generate a random word with 10 characters
-	randomWord := generateRandomWord(10)
-	// Print the random word to the console
-	fmt.Println(randomWord)
+func compareStrings(str1 string, str2 string) bool {
+	if str1 == str2 {
+		return true
+	} else {
+		return false
+	}
 }
 
-func generateRandomWord(length int) string {
-	// Define a slice of runes containing all the letters of the alphabet
-	letters := []rune("abcdefghijklmnopqrstuvwxyz")
-	// Seed the random number generator with the current time
-	rand.Seed(time.Now().UnixNano())
-	// Create a slice of runes with the specified length
-	b := make([]rune, length)
-	// Fill the slice with random letters from the letters slice
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	// Convert the slice of runes to a string and return it
-	return string(b)
+func main() {
+	fmt.Println(compareStrings("hello", "hello")) // true
+	fmt.Println(compareStrings("hello", "world")) // false
 }
